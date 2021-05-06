146e95ec3ab11021fa789b4b1bb32743a0eb7398
Cameryn Duarte <82423065+camerynd@users.noreply.github.com>
2021-05-06 12:41:22 -0600

    Add files via upload

---
+++ Chapter02_Q9.cpp
@@ -0,0 +1,35 @@
+/*
+Chapter02_Q9.cpp
+Duarte, Cameryn
+CSC119-142 Introduction to Programming C++ Spring 2021
+02/04/2021
+This program calculates the number of calories consumed in cookies.
+*/
+//tells the processor to include the input output functions
+#include <iostream>
+//used to differentiate similar functions
+using namespace std;
+//declaring main function
+int main()
+{
+//declaring constants
+    const double WHOLEBAG = 30, SERVINGNUM = 10, SERVINGCAL = 300;
+//declaring variables
+    double cookiesEaten, cookieNum, cookieCal, calsEaten;
+//explaining what the program does
+    cout<< "This program calculates the amount of calories you've consumed in cookies." << endl << endl;
+//asking for user input
+    cout<< "Enter number of cookies eaten: ";
+//assigning user input to variable
+    cin>> cookiesEaten;
+//processing and assigning values to other variables using user input and constants
+    cookieNum = WHOLEBAG / SERVINGNUM;
+    cookieCal = SERVINGCAL / cookieNum;
+    calsEaten = cookieCal * cookiesEaten;
+
+    cout<< endl << endl;
+//displaying output
+    cout<< "You have consumed " << calsEaten << " calories.";//output
+//signaling end of program
+    return 0;
+}
